68196a46cc3f093aa2e0cbc429e5c288ec04eada
Rafael Jung <rafajmeu@gmail.com>
2023-10-25 13:58:48 +0000

    API fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,41 +8,49 @@ import { url } from 'inspector'
 import Image from 'next/image'
 
 import { PrismaClient, receivedalerts } from '@prisma/client'
-import { useEffect, useState } from 'react'
+import { Key, useEffect, useState } from 'react'
 
 import { prisma } from '@/lib/prisma'
 
-export default function Home() {
-  const [filtro, setFiltro] = useState<receivedalerts[]>([])
 
-  const [cor, setCor] = useState('BLUE')
+const getBaseData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users") 
+    return res.json()
+}
 
-  useEffect(() => {
-    async function getData() {
-      const data = await (await fetch(`/api?cor=${cor}`)).json()
-      setFiltro(data)
-      console.log(data)
-    }
-    getData()
-  }, [cor])
 
-  console.log(filtro)
+
+export default async function Home() {
+  // const [filtro, setFiltro] = useState<receivedalerts[]>([])
+
+  // const [cor, setCor] = useState('BLUE')
+
+  const alertas = await getBaseData()
+
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     const data = await (await fetch(`/api?cor=${cor}`)).json()
+  //     setFiltro(data)
+  //     console.log(data)
+  //   }
+  //   getData()
+  // }, [cor])
+
+  console.log(alertas)
 
   return (
     <main className="">
-      <button onClick={() => setCor('red')}>Filter by Red</button>
-      <button onClick={() => setCor('green')}>Filter by Green</button>
-      <button onClick={() => setCor('blue')}>Filter by Blue</button>
       <Navbar></Navbar>
       <div className="flex flex-row">
         <div className="min-h-screen w-1/5 h-screen mr-4 hidden lg:block">
-          <SideMenu setCor={setCor}></SideMenu>
+          {/* <SideMenu setCor={setCor}></SideMenu> */}
         </div>
         <div className="block lg:hidden rounded-xl">
           <SideBarResponsive></SideBarResponsive>
         </div>
         <div className=" lg:p-0 p-12">
-          {filtro.map(item => (
+          {alertas.map((item: any) => (
             <Aviso
               title={item.alertid!}
               description={item.description!}
